9046dc8677a9a4c9378c5669c20e77525a0a5e77
niharikavermaa01 <niharikaverm01k@gmail.com>
2025-10-25 12:34:47 +0530

    adding joi package code

---
+++ joiSchema.js
@@ -0,0 +1,15 @@
+const Joi = require('joi');
+
+module.exports.listingSchema = Joi.object({
+    newListing: Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        location: Joi.string().required(),
+        country: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.string().allow("",null),
+    }).required()
+});
+
+//We use Joi to validate our form submisssion request to check 
+//if user did not entered a detail in any column that was required so this kind of errors our joi will handle 

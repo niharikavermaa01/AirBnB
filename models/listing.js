67557566f795f67b1b47392610bf822bb263a5fc
Niharika <niharika@Niharikas-MacBook-Air.local>
2025-12-12 15:11:41 +0530

    I have made some changes

---
+++ models/listing.js
@@ -0,0 +1,44 @@
+const { ref } = require("joi");
+const mongoose = require ("mongoose");
+const Schema = mongoose.Schema;
+const Review = require("./review.js");
+
+
+const listingSchema = new Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+    },
+    image:{
+        type: String,
+        default: "https://unsplash.com/photos/city-skyline-across-the-water-from-a-ferry-deck-Heu_fJQNTf0",
+        set: (v)=> v=== "" ? "https://unsplash.com/photos/two-surfers-wait-for-waves-near-a-rocky-coastline-MA0nBwOmuwQ" : v
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    location: {
+        type: String
+    },
+    country: {
+        type: String
+    },
+    reviews:[{
+        type: Schema.Types.ObjectId,
+        ref: "Review"
+    }]
+});
+
+listingSchema.post("findOneAndDelete", async(listing)=>{
+    if(listing){
+        await Review.deleteMany({_id:{$in: listing.reviews}});
+    }
+});
+
+
+const Listing = mongoose.model("Listing",listingSchema);
+module.exports= Listing;
